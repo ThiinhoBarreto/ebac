a9926a220ba950939091718f32f374e1ec609870
Thiinho Barreto <barreto.fatura@outlook.com>
2023-04-13 09:34:08 -0300

    atualizações no código (parseInt)

--- script.js
+++ script.js
@@ -5,21 +5,16 @@ const mensagem = document.getElementById('mensagem');
 
 
 form.addEventListener('submit', function(event) {
-    // aqui, podemos realizar as ações que desejamos quando o formulário é submetido
-    // por exemplo, podemos impedir o envio padrão do formulário com preventDefault():
     event.preventDefault();
 
-    if (numB.value > numA.value) {
-
+    if (parseInt(numB.value) > parseInt(numA.value)) {
         mensagem.style.backgroundColor = '#9dfdc8';
         mensagem.textContent = 'Bom trabalho! O valor B é maior que o valor A!'
         mensagem.style.display = 'inline'
 
-    }else if (numB.value < numA.value) {
+    }else if (parseInt(numB.value) < parseInt(numA.value) || parseInt(numB.value) == parseInt(numA.value)) {
         mensagem.style.backgroundColor = '#ffabab';
         mensagem.textContent = 'Erro!! O valor B precisa ser maior que o valor A'
         mensagem.style.display = 'inline'
-    }else {
-        mensagem.style.display = 'none'
     }
     });
